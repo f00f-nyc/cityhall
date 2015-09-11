1b068dee93cca81a0a6cb8bdc0455a3565aa89a5
Alex Popa <alex.popa@dealertrack.com>
2015-09-11 16:10:18 -0400

    Remove postgres settings

--- cityhall/cityhall/settings.py
+++ cityhall/cityhall/settings.py
@@ -112,18 +112,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Parse database configuration from $DATABASE_URL
-DATABASES['default'] =  dj_database_url.config()
-
-# Enable Connection Pooling (if desired)
-DATABASES['default']['ENGINE'] = 'django_postgrespool'
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
