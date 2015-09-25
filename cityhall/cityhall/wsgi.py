4677f9b956d662cf5fd7c21914b2c9326763aec1
Alex Popa <alex.popa@gmail.com>
2015-09-25 11:47:33 -0400

    Add files neccesary for running on heroku

--- cityhall/cityhall/wsgi.py
+++ cityhall/cityhall/wsgi.py
@@ -22,9 +22,12 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cityhall.settings")
 
-application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
+
+
+application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
