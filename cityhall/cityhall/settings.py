a2afa9c15adb8eb7f5accb6dd78d0feb4c35dd5f
Alex Popa <alex.popa@gmail.com>
2020-08-23 21:00:55 -0400

    Attempt to fix STATIC_ROOT mis-setting

--- cityhall/cityhall/settings.py
+++ cityhall/cityhall/settings.py
@@ -112,7 +112,8 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = ''
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
